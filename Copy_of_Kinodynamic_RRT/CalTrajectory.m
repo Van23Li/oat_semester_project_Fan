a41c5609dac397dbae17295bcba2ed6123dcc699
Van23Li <2178283152@qq.com>
2022-10-27 22:00:31 +0200

    add anime

--- Copy_of_Kinodynamic_RRT/CalTrajectory.m
+++
@@ -1,4 +0,0 @@
-function Result = CalTrajectory(x1, x2, cfg)
-    T = Dist_kd(x1, x2, cfg);
-    Result = Steer(x1, x2, T, cfg); %[a_1, v_limit, a_2, t_1, t_v, t_2]
-end
--- Copy_of_Kinodynamic_RRT/try_KD.m
+++
@@ -1,7 +0,0 @@
-clear all
-
-load fisheriris
-X = meas;
-[n,k] = size(X);
-
-Mdl1 = KDTreeSearcher(X);
